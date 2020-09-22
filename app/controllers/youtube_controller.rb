87c6ac6e93428cd8a95d444c3e1b2f7404cd4d94
hori756 <horikoshi756@gmail.com>
2020-09-22 17:13:13 +0900

    検索機能トップページ作成

--- app/controllers/youtube_controller.rb
+++ app/controllers/youtube_controller.rb
@@ -20,36 +20,9 @@ class YoutubeController < ApplicationController
     service.list_searches(:snippet, opt)
   end
   def search
-    if !params[:keyword].blank?
-        require 'google/apis/youtube_v3'
-        youtube = Google::Apis::YoutubeV3::YouTubeService.new
-        youtube.key = GOOGLE_API_KEY
-        youtube_search_list = youtube.list_searches("id,snippet", type: "video",
-                                                                q: params[:keyword],
-                                                                max_results: 1)
-        search_result = youtube_search_list.to_h
-        @movies = search_result[:items]
-        if !@movies.nil?
-            respond_to do |format|
-                format.html { render 'youtube'}
-                format.js
-            end
-        else
-            flash[:warning] = "検索結果はありません"
-            respond_to do |format|
-                format.html { render 'youtube'}
-                format.js
-            end
-        end
-    else 
-        flash.now[:warning] = "検索に空白は使えません"
-        respond_to do |format|
-            format.html { render 'youtube'}
-            format.js
-        end
-    end
-end
-  def index
     @youtube_data = find_videos('スポーツ')
+  end
+  def index
+    
   end
 end
