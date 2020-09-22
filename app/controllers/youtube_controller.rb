c3d806f875e74f49a5d0ad491f3d9481c77ffea3
hori756 <horikoshi756@gmail.com>
2020-09-22 12:02:20 +0900

    コントローラー雛形

--- app/controllers/youtube_controller.rb
+++ app/controllers/youtube_controller.rb
@@ -1,4 +1,26 @@
 class YoutubeController < ApplicationController
+  GOOGLE_API_KEY = "AIzaSyCwR0JztvWLQncwUInVRQX_dP8zHM-22Qs"
+  #Rails.application.credentials.google[:api_key]
+
+  def find_videos(keyword, after: 1.months.ago, before: Time.now)
+    #require 'google/apis/youtube_v3'
+    service = Google::Apis::YoutubeV3::YouTubeService.new
+    service.key = GOOGLE_API_KEY
+
+    next_page_token = nil
+    opt = {
+      q: keyword,
+      type: 'video',
+      max_results: 2,
+      order: :date,
+      page_token: next_page_token,
+      published_after: after.iso8601,
+      published_before: before.iso8601
+    }
+    service.list_searches(:snippet, opt)
+  end
+
   def index
+    @youtube_data = find_videos('アニメ')
   end
 end
