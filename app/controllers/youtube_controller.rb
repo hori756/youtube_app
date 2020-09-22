f7eac8aa4a515287713b126d22115177a7d89913
hori756 <horikoshi756@gmail.com>
2020-09-22 15:52:09 +0900

    search作成

--- app/controllers/youtube_controller.rb
+++ app/controllers/youtube_controller.rb
@@ -11,7 +11,7 @@ class YoutubeController < ApplicationController
     opt = {
       q: keyword,
       type: 'video',
-      max_results: 2,
+      max_results: 1,
       order: :date,
       page_token: next_page_token,
       published_after: after.iso8601,
@@ -19,8 +19,37 @@ class YoutubeController < ApplicationController
     }
     service.list_searches(:snippet, opt)
   end
-
+  def search
+    if !params[:keyword].blank?
+        require 'google/apis/youtube_v3'
+        youtube = Google::Apis::YoutubeV3::YouTubeService.new
+        youtube.key = GOOGLE_API_KEY
+        youtube_search_list = youtube.list_searches("id,snippet", type: "video",
+                                                                q: params[:keyword],
+                                                                max_results: 1)
+        search_result = youtube_search_list.to_h
+        @movies = search_result[:items]
+        if !@movies.nil?
+            respond_to do |format|
+                format.html { render 'youtube'}
+                format.js
+            end
+        else
+            flash[:warning] = "検索結果はありません"
+            respond_to do |format|
+                format.html { render 'youtube'}
+                format.js
+            end
+        end
+    else 
+        flash.now[:warning] = "検索に空白は使えません"
+        respond_to do |format|
+            format.html { render 'youtube'}
+            format.js
+        end
+    end
+end
   def index
-    @youtube_data = find_videos('アニメ')
+    @youtube_data = find_videos('スポーツ')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   get 'youtube/index'
   root to: "youtube#index"
+  get 'youtube/search'
+  post 'youtube/search'
 end
