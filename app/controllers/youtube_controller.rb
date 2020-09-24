83132009e3c9b0e62634368c5cbe4fcc26851f66
hori756 <horikoshi756@gmail.com>
2020-09-24 18:53:27 +0900

    video_id出力

--- app/controllers/youtube_controller.rb
+++ app/controllers/youtube_controller.rb
@@ -11,16 +11,31 @@ class YoutubeController < ApplicationController
     opt = {
       q: keyword,
       type: 'video',
-      max_results: 1,
+      max_results: 2,
       order: :viewCount,
       page_token: next_page_token,
       published_after: after.iso8601,
       published_before: before.iso8601
     }
-    service.list_searches(:snippet, opt)
+    results = service.list_searches(:snippet, opt)
+    
+    #@video_title = response[:items][0][:snippet][:title]
+    #video_id = response[:items][0][:id][:videoId]
   end
+
+  def count_videos(youtube_data)
+
+    results_items = youtube_data.to_h
+    video = results_items[:items][0][:id][:video_id]
+    
+      #video_cnt_get = service.list_videos(:statistics, id: video_id)
+      #video_cnt_get_h = video_cnt_get.to_h  
+
+  end
+
   def search
     @youtube_data = find_videos(params[:keyword])
+    @youtube_data_count = count_videos(@youtube_data)
   end
   def index
     
